6117e8c6dead396271181f6b99c20ec8dec11ada
virusdefender <virusdefender@outlook.com>
2021-05-26 19:14:02 +0800

    fix wrong MaxBytesPerChar

--- innodb-java-reader/src/main/java/com/alibaba/innodb/java/reader/schema/TableDef.java
+++ innodb-java-reader/src/main/java/com/alibaba/innodb/java/reader/schema/TableDef.java
@@ -175,7 +175,7 @@ public class TableDef {
     if (column.isVariableLength()) {
       variableLengthColumnList.add(column);
       variableLengthColumnNum++;
-    } else if (CHAR.equals(column.getType()) && maxBytesPerChar > 1) {
+    } else if (CHAR.equals(column.getType()) && column.getMaxBytesPerChar() > 1) {
       // 多字符集则设置为varchar的读取方式
       column.setVarLenChar(true);
       variableLengthColumnList.add(column);
