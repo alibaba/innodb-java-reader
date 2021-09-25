acc478e7c207b49c1888d9fcaa85ae7bbab07881
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-25 16:31:30 +0800

    Delete innodb-java-reader directory

--- innodb-java-reader/src/main/java/com/alibaba/innodb/java/reader/page/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (C) 1999-2019 Alibaba Group Holding Limited
- */
-
-/**
- * Innodb page related core classes.
- */
-package com.alibaba.innodb.java.reader.page;
--- innodb-java-reader/src/test/resources/testsuite/mysql56/simple/empty_table.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `empty_table`;
-CREATE TABLE `empty_table` (
-`key` int(11) NOT NULL,
-`value` varchar(288) DEFAULT NULL,
-PRIMARY KEY  (`key`)
-) ENGINE=InnoDB;
